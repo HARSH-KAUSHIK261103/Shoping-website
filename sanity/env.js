0c1b9c06865becfa54e3ded1e01ff2582122e8d6
HARSH-KAUSHIK261103 <harshkaushik261103@gmail.com>
2024-11-05 20:53:12 +0530

    first commit

---
+++ sanity/env.js
@@ -0,0 +1,8 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-02-26";
+
+export const dataset = "production";
+export const projectId = "apj68o3s";
+export const useCdn = false;
+export const token =
+  "skxiFsr0xsJqOg6CMy3z9HI0abVknWzn279P0sUA3chfvJYPeA6LktJZGa8wXRaOGkdcxZTQ9C8k3T6nFhSnDSOQCBmhMbWGRmJZs9CTXu9ehqUJuji7LuaCPY64frwipCV9Jde3Ef2VsqRZGbYbh0tjQSpEgeJBZRVRL7psnnS3EKIybKSJ";
